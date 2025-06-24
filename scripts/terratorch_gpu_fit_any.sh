d59ba7a7658d099c4b941bdf48df6c1fe5f37649
Caroline Arnold <arnold@dkrz.de>
2025-06-24 14:28:48 +0200

    use ddp

--- scripts/terratorch_gpu_fit_any.sh
+++ scripts/terratorch_gpu_fit_any.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 #SBATCH -A ka1176
 #SBATCH -p gpu
-#SBATCH --mem=100G
-#SBATCH --gres=gpu:1
+#SBATCH --exclusive
+#SBATCH --gres=gpu:4
 #SBATCH --nodes=1
 #SBATCH --time=12:00:00
-#SBATCH --cpus-per-task=10
 #SBATCH --constraint=a100_80
 #SBATCH -o scripts/slurm/slurm-%j.out
 
@@ -14,6 +13,6 @@ conda activate terramind
 
 # pass e.g. config/arctic_sea_ice/sic.yaml as the first argument
 
-terratorch fit -c $1 --custom_modules_path="/work/ka1176/caroline/gitlab/terramind-demo" 
+terratorch fit -c $1 --custom_modules_path="/work/ka1176/caroline/gitlab/terramind-demo"
 
 wandb artifact cache cleanup --remove-temp 0GB
